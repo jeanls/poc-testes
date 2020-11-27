90dd72d54c4d80df881f3fc363217cc683cfcb0f
jean <jean.leal@zup.com.br>
2020-11-27 10:31:15 -0300

    initial commit

---
+++ src/main/java/com/testes/poctest/PoctestApplication.java
@@ -0,0 +1,13 @@
+package com.testes.poctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoctestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoctestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testes/poctest/PoctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testes.poctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoctestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
